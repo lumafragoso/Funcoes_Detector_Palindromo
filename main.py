63a23681e367b89752de60ad07197c4483d38a41
Luma Fragoso <85634234+lumafragoso@users.noreply.github.com>
2021-07-17 00:57:12 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+Um palíndromo é uma palavra que se soletra da mesma forma nos dois sentidos, como “osso” e “reviver”. Essa mesma característica pode ser aplicada a frases, retirando os espaços entre as palavras. Exemplo de frases palíndromos:
+
+apos a sopa
+a sacada da casa
+a torre da derrota
+o lobo ama o bolo
+anotaram a data da maratona
+Construa a função inverso(texto) que receba um texto qualquer como argumento e retorne esse texto em ordem inversa. Em seguida, use esta função para checar se um texto é palíndromo ou não.
+
+Dica: em Python, assim como em outras linguagens, todo texto (ou string) é uma lista ou um vetor de caracteres. O acesso a cada caractere do texto é feito através do índice deste vetor, o qual inicia em 0 (zero) e vai até len(vetor) - 1
